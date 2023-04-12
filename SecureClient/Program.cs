cd38a6538a38d6dfad7061ea62e23e338fa084dc
Muhammet Çeneli <muhammetceneli@gmail.com>
2023-04-12 14:50:50 +0300

    SecureApi + Client Yazıldı

---
+++ SecureClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace SecureClient{
+    class Program{
+        static void Main(string[] args){
+            AuthConfig config = AuthConfig.ReadJsonFromFile("appsettings.json");
+
+            Console.WriteLine($"Authority: {config.Authority}");
+        }
+    }
+}
